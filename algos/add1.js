ecbfab96f6750f1878706c91c496161fdc2705dd
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-02-15 09:06:45 -0500

    add up to and at least

--- algos/add1.js
+++ algos/add1.js
@@ -6,4 +6,14 @@ function addUpTo(n) {
     return total;
 }
 
+let t1 = performance.now()
+addUpTo(1000000000)
+var t2 = performance.now()
+console.log(`Time Elapsed: ${(t2-t1) / 1000} seconds.`)
+
 console.log(addUpTo(13))
+
+// Counting Operations
+    // one plus sign, doesn't mean one operation. It's n operations (but also = is another operation, so it's like n*2 operations) ++ n additions and n assignments; let total = 0 is 1 assignment, let i = 1 is one assignment, <+ n comparisons
+    // not a static number of operations 5n + 2 operations, regardless of exact number, the number of operations grows roughly in proportion with n
+
---
+++ algos/add2.js
@@ -0,0 +1,9 @@
+
+function addUpToAgain(n) {
+    return n * (n+1) / 2
+}
+
+console.log(addUpToAgain(13))
+
+// Counting Operations
+    // 1 multiplication, 1 addition, 1 division, 3 simple operations whether n is 1 or 1 billion
