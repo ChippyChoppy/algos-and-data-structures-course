4ee0619c95e6eab1df2f26879fd5d2c94d047fa5
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-04-11 13:27:05 -0400

    unique values all by myself, diff method than colt

--- algos/countUniqueVal.js
+++ algos/countUniqueVal.js
@@ -5,3 +5,39 @@
 // countUniqueValues([]) // 0
 // countUniqueValues([-2,-1,-1,0,1]) // 4
 
+function countUniqueValues(arr) {
+    let left = 0
+    let right = 1
+    while (left < right && right < arr.length) {
+        if (arr[left] === arr[right]) {
+            arr.splice(right, 1)
+        } else {
+            left++, right ++
+        } 
+    }
+    return arr.length
+}
+
+console.log(countUniqueValues([1,1,1,2,2,3,4,5,5]))
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
+
+// [1,1,1,2,2,3,4,5,5]
+
+/* 
+1 vs 1
+[1,1,2,2,3,4,5,5]
+1 vs 1 
+[1,2,2,3,4,5,5] 
+1 vs 2
+[1,2,2,3,4,5,5] 
+2 vs 2 
+[1,2,3,4,5,5] 
+2 vs 3
+[1,2,3,4,5,5] 
+3 vs 4
+[1,2,3,4,5,5] 
+4 vs 5
+[1,2,3,4,5,5] 
+5 vs 5
+[1,2,3,4,5] 
+*/
