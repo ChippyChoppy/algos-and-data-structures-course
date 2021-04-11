9b7348fb681bff2660bfc86ec2f2eda34880be83
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-04-11 14:04:03 -0400

    first solution, not sliding window & quadratic time, second solutions is sliding window and o(n)

--- algos/maxSubarraySum.js
+++ algos/maxSubarraySum.js
@@ -9,6 +9,9 @@ console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
 console.log(maxSubarraySum([],4)) // null
 */
 
+/* nested loop, probably  not best solution 
+    BigO(n^2) this is not a sliding window
+*/
 function maxSubarraySum(arr, n) {
     if ( n > arr.length) {
         return null;
@@ -22,11 +25,33 @@ function maxSubarraySum(arr, n) {
         if (temp > max) {
             max = temp;
         }
+        console.log(temp, max)
     }
     return max;
 }
-console.log(maxSubarraySum([1,2,5,2,8,1,5], 2))
-console.log(maxSubarraySum([1,2,5,2,8,1,5], 4)) // 17
-console.log(maxSubarraySum([4,2,1,6],1)) // 6
-console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
-console.log(maxSubarraySum([],4)) // null
+// console.log(maxSubarraySum([1,2,5,2,8,1,5], 2))
+// console.log(maxSubarraySum([1,2,5,2,8,1,5], 4)) // 17
+// console.log(maxSubarraySum([4,2,1,6],1)) // 6
+// console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
+// console.log(maxSubarraySum([],4)) // null
+
+function maxSubarraySum2(arr, n) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < n) return null;
+    for (let i = 0; i < n; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = n; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - n ] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+console.log(maxSubarraySum2([1,2,5,2,8,1,5], 2)) // 10
+console.log(maxSubarraySum2([1,2,5,2,8,1,5], 4)) // 17
+console.log(maxSubarraySum2([4,2,1,6],1)) // 6
+console.log(maxSubarraySum2([4,2,1,6,2],4)) // 13
+console.log(maxSubarraySum2([],4)) // null
