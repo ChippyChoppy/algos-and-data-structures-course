827a57bcf618bd823a986763799381cb74e9ef2b
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-04-12 02:49:20 -0400

    worked it out

--- algos/isSubsequence.js
+++ algos/isSubsequence.js
@@ -14,17 +14,15 @@ Space Complexity - O(1)
 
 function isSubsequence(str1, str2) {
     if (str1.length > str2.length) return false;
+    if (!str1) return true;
     let j = 0;
     let i = 0;
-    while ( i < str1.length ) {
-        if (str1[i] === str2[j]) {
-            i++, j++
-        } else if (str1[i] !== str2[j]) {
-            j++
-        } else return false;
-        
+    while ( j < str2.length ) {
+        if (str1[i] === str2[j]) i++;
+        if (i === str1.length) return true;
+        j++;
     }
-    return true
+    return false
 }
 
 console.log(isSubsequence('hello', 'hello world')) // true
