ffa34c1e83e8f75006938b0a9d58b68e5a748944
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-04-12 02:08:40 -0400

    bam, average pair, multiple pointers, no hints

--- algos/averagePair.js
+++ algos/averagePair.js
@@ -12,10 +12,20 @@ console.log(averagePair([],4)) // false
 */
 
 function averagePair(arr, targetAve) {
+    let targetNum = targetAve * 2 
+    let beg = 0
+    let end = arr.length - 1
     if (arr.length < 2) return false;
-    else if (!Number.isInteger(targetAve * 2)) return false;
+    if (!Number.isInteger(targetNum)) return false;
 
-    
+    while (beg < end) {
+        if (arr[beg] + arr[end] === targetNum) return true
+        else if (arr[beg] + arr[end] > targetNum) {
+            end--
+        } else if (arr[beg] + arr[end] < targetNum) {
+            beg++
+        }
+    }
 }
 console.log(averagePair([1,2,3],2.5)) // true
 console.log(averagePair([1,3,3,5,6,7,10,12,19],8)) // true
