92296895ff352a9c34099a022f7a7c4754768843
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-05-30 22:56:57 -0400

    findLongestSubstring

---
+++ algos/findLongestSubstring.js
@@ -0,0 +1,12 @@
+/* 
+Sliding Window - findLongestSubstring
+
+Write a function called findLongestSubstring, which accepts a string and returns the lenght of the longest substring with all distinct characters.
+
+findLongestSubstring('') // 0
+findLongestSubstring('rithmschool') // 7
+findLongestSubstring('thisisawesome') // 6
+findLongestSubstring('thecatinthehat') // 7
+findLongestSubstring('bbbbbbb') // 1
+findLongestSubstring('longestsubstring') // 8
+*/
--- algos/minSubArrayLen.js
+++ algos/minSubArrayLen.js
@@ -1,6 +1,6 @@
 // Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.
 
-// This function should return the minimal lenght of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead. 
+// This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead. 
 
 /* Examples:
 minSubArrayLen([2,3,1,2,4,3], 7)
@@ -13,5 +13,5 @@ minSubArrayLen([1,4,16,22,5,7,8,9,10], 95)
 */
 
 function minSubArrayLen(arr, int) {
-    
+
 }
