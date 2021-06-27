e42f9087979f224efc93fd3f269079858624111f
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-06-27 17:28:14 -0400

    base power baby

---
+++ algos/Recursion Prax/power.js
@@ -0,0 +1,14 @@
+/* Write a function called power which accepts a base and an exponent. The funtion should return the power of the base to the exponent. This function should mimic the functionality of Math.pow() - do not worry about negative bases and exponents
+*/
+
+function power(base, exp) {
+    if (exp === 0) return 1;
+    return base * power(base, exp-1);
+}
+
+console.log(power(2, 8))
+
+/*
+2 * [2^7]
+    
+*/
