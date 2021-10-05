1635dc0765d388626da04fc9313658d1d59b3f8b
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-10-05 11:28:18 -0400

    flatten

--- algos/RecursionPrax/flatten.js
+++ algos/RecursionPrax/flatten.js
@@ -8,13 +8,17 @@
 function flatten(arr) {
     let flatArray = [];
     for (let i = 0; i < arr.length; i++) {
+        console.log(flatArray, "line 11")
+        console.log(Array.isArray(arr[i]), arr[i], "i:", i, "line 12")
         if (Array.isArray(arr[i])) {
             flatArray = flatArray.concat(flatten(arr[i]))
+            console.log(flatArray, "line 16")
         } else {
             flatArray.push(arr[i])
+            console.log(flatArray, "line 19")
         }
     }
     return flatArray
 }
 
-console.log(flatten([[1],[2],[3]]))
+console.log(flatten([[[[[[[[[[1]]]]]]]]],[[[[2]]]],3,[4,5],[6,7]]))
