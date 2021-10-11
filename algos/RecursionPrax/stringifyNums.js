7f627011f0feeb04f7ca41bdf8b8fe873fb2c352
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-10-11 11:45:03 -0400

    stringify nums

---
+++ algos/RecursionPrax/stringifyNums.js
@@ -0,0 +1,32 @@
+/**
+ * Write a function called stringifyNumbers which takes in an object
+ * and finds all of the values which are numbers and converts them to strings.
+ * Recursion would be a great way to solve this!
+ */
+
+/*
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+*/
+
+function stringifyNumbers(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'object') {
+            stringifyNumbers(obj[key])
+        }
+        else if (typeof obj[key] === 'number') {
+            obj[key] = obj[key].toString()
+        }
+    }
+    return obj
+}
+
